d1bad6b8c53f062de70753975ea2cc8d84fd705a
paulohenriquerodrigues <rodriguespaulo1997@gmail.com>
2018-06-30 17:17:15 -0300

    Cadastro de Usuários Finalizado!

--- front-end-dsw/src/telas/Login.js
+++ front-end-dsw/src/telas/Login.js
@@ -54,17 +54,40 @@ class Login extends React.Component {
 
     cadastraUsuario(){
      var usuario = new Object();
+     var endereco = new Object();
      usuario.cpf = document.getElementById('cpfCadastro').value;
      usuario.nomeCompleto = document.getElementById('nome').value;
      usuario.senha = document.getElementById('senhaCadastro').value;
      usuario.email = document.getElementById('email').value;
-     usuario.administrador = document.getElementById('administrador').value;
+     if(document.getElementById('administrador').value === 'on'){
+         usuario.administrador = true;
+     }else{
+         usuario.administrador = false;
+     }
+     endereco.pais = document.getElementById('pais').value;
+     endereco.estado = document.getElementById('estado').value;
+     endereco.cidade = document.getElementById('cidade').value;
+     endereco.bairro = document.getElementById('bairro').value;
+     endereco.rua = document.getElementById('rua').value;
+     endereco.cep = document.getElementById('cep').value;
+     endereco.numeroCasa = document.getElementById('numero').value;
 
+     usuario.endereco = endereco;
 
-
-        console.log(JSON.stringify(usuario));
-
-
+     fetch("http://localhost:8080/API_REST_DSW/webresources/Usuario", {
+            method: "PUT",
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify(usuario)
+        }).then(function (result) {
+            alert("Usuário Cadastrado com sucesso!");
+            ReactDOM.render(<BrowserRouter>
+                    <Template/>
+                </BrowserRouter>,
+                document.getElementById('root'));
+        });
     }
 
 
@@ -96,6 +119,7 @@ class Login extends React.Component {
                         <Input type="text" name="estado" id="estado" placeholder="Estado"/><br/>
                         <Input type="text" name="pais" id="pais" placeholder="País"/><br/>
                         <Input type="number" name="cep" id="cep" placeholder="CEP"/><br/>
+                        <Input type="checkbox" id="administrador"/>Administrador
                         <Button onClick={this.cadastraUsuario.bind(this)}  color="success">Cadastrar</Button>
 
                     </Col>
